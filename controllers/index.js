6ff7f0144dc25334ad8219d63ee86e365eac8298
Erica <erica.trenholm48@gmail.com>
2022-04-20 17:44:57 -0400

    finalize setup

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,17 @@
 const router = require('express').Router();
 
 // routes
-    // const apiRoutes = require('./api');
-    // const homeRoutes = require('./home-routes.js');
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
 // connect routes to router
-    // router.use('/api', apiRoutes);
-    // router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
 });
 
-// export router
 module.exports = router;
